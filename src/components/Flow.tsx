d113ef454eb0ae56c1c1736c98c7cc55d1f9e531
Kunal <kunal@lica.world>
2025-06-01 13:03:03 +0530

    Add basic nodes and edges and callback handlers

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -1,14 +1,44 @@
-import { ReactFlow, Controls, Background } from '@xyflow/react'
+import {
+	ReactFlow,
+	Controls,
+	Background,
+	applyEdgeChanges,
+	applyNodeChanges,
+	addEdge,
+	type OnEdgesChange,
+	type OnNodesChange,
+	type OnConnect
+} from '@xyflow/react'
 import '@xyflow/react/dist/style.css'
-import { useContext } from 'react'
+import { useCallback, useContext } from 'react'
 import { DataContext } from '../context'
 
 function Flow() {
-  const {nodes, edges} = useContext(DataContext)
+	const { nodes, edges, setNodes, setEdges } = useContext(DataContext)
+
+	const onNodesChange: OnNodesChange = useCallback(
+		changes => setNodes(nds => applyNodeChanges(changes, nds)),
+		[setNodes]
+	)
+	const onEdgesChange: OnEdgesChange = useCallback(
+		changes => setEdges(eds => applyEdgeChanges(changes, eds)),
+		[setEdges]
+	)
+	const onConnect: OnConnect = useCallback(
+		connection => setEdges(eds => addEdge(connection, eds)),
+		[setEdges]
+	)
 
 	return (
 		<div className='w-full h-screen'>
-			<ReactFlow nodes={nodes} edges={edges} fitView>
+			<ReactFlow
+				nodes={nodes}
+				edges={edges}
+				fitView
+				onNodesChange={onNodesChange}
+				onEdgesChange={onEdgesChange}
+				onConnect={onConnect}
+			>
 				<Background />
 				<Controls />
 			</ReactFlow>
--- src/context/DataContextProvider.tsx
+++ src/context/DataContextProvider.tsx
@@ -3,8 +3,17 @@ import { useState } from 'react'
 import { DataContext } from '.'
 
 export const DataContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [nodes, setNodes] = useState<Node[]>([])
-  const [edges, setEdges] = useState<Edge[]>([])
+  const [nodes, setNodes] = useState<Node[]>([
+		{
+			id: '1',
+			position: { x: 0, y: 0 },
+			data: { label: 'Hello' }
+		},
+		{ id: '2', position: { x: 100, y: 100 }, data: { label: 'World' } }
+	])
+  const [edges, setEdges] = useState<Edge[]>([
+    { id: '1-2', source: '1', target: '2' },
+  ])
 
   return (
     <DataContext.Provider value={{ nodes, edges, setNodes, setEdges }}>
