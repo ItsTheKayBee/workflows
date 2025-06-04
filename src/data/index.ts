c2f4691181a57c35f055be6cc49cfd03b8de55cb
Kunal <kunal@lica.world>
2025-06-04 12:32:27 +0530

    remove dynamic import

--- src/data/index.ts
+++ src/data/index.ts
@@ -9,4 +9,12 @@ export const FlowType = {
 
 export const getData = async (
 	type: FlowType
-): Promise<{ nodes: Node[]; edges: Edge[] }> => import(`./${type}`)
+): Promise<{ nodes: Node[]; edges: Edge[] }> => {
+	if (type === FlowType.simple) {
+		return import('./simple.ts');
+	} else if (type === FlowType.complex) {
+		return import('./complex.ts');
+	}
+
+	throw new Error(`Unknown flow type: ${type}`);
+}
