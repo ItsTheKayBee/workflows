e84bada7a48f043b499fb0f412cd6da9726a4837
Kunal <kunal@lica.world>
2025-06-04 12:07:46 +0530

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: '/workflows/' // Set base for GitHub Pages (repo name)
+	base: '/workflows/'
 })
